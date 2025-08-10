9fc0c1aa32305d17818575b836e6b0c16228f7b3
jngbn <[user email]>
2025-08-10 23:05:34 +0900

    섹션 8 웹 계츨 개발 7. 변경 감지와 병합

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,11 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class ItemRepository {
-    private final EntityManager em;
+
+    @PersistenceContext
+    EntityManager em;
+
+//    private final EntityManager em;
     public void save(Item item){
         if(item.getId()==null){
             em.persist(item);
--- src/main/java/jpabook/jpashop/web/ItemController.java
+++ src/main/java/jpabook/jpashop/web/ItemController.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -59,19 +60,31 @@ public class ItemController {
         model.addAttribute("form", form);
         return "items/updateItemForm";
     }
+//    /**
+//     * 상품 수정
+//     */
+//    @PostMapping(value = "/items/{itemId}/edit")
+//    public String updateItem(@ModelAttribute("form") BookForm form) {
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//        itemService.saveItem(book);
+//        return "redirect:/items";
+//    }
     /**
-     * 상품 수정
+     * 상품 수정, 권장 코드
      */
     @PostMapping(value = "/items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form) {
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
-        itemService.saveItem(book);
+    public String updateItem(@PathVariable Long itemId,
+                             @ModelAttribute("form") BookForm form) {
+        itemService.updateItem(itemId, form.getName(), form.getPrice(),
+                form.getStockQuantity());
         return "redirect:/items";
     }
+
+
 }
