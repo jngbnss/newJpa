730a4145adde40d5035bc065853c613a3e91ae69
jngbn <[user email]>
2025-08-11 09:55:40 +0900

    섹션 2 API 개발 기본 7. 회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,9 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
@@ -62,6 +65,34 @@ public class MemberApiController {
         private Long id;
         private String name;
     }
+
+    @GetMapping("/api/v1/member")
+    public List<Member>membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/member")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
+
+
 
 
 }
